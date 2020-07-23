c4f916e736ef3921c0966ada96bc9e715626e981
Eric Enns <eric.enns@gmail.com>
2020-07-23 10:07:03 -0500

    Added in comments for basic assembly worflow

--- proksee/commands/cmd_assemble.py
+++ proksee/commands/cmd_assemble.py
@@ -1,5 +1,9 @@
 import click
 import os
+# import platform detection
+# import quality module
+# import organism detection
+# import assembler
 
 
 @click.command('assemble',
@@ -17,3 +21,15 @@ def cli(ctx, forward, reverse, output_dir):
 
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
+
+    # Step 1: Platform detection
+    # Pass forward and reverse datasets to platform detection module and ensure that both files are of the same platform
+
+    # Step 2: Quality Check
+    # Pass forward and reverse datasets to quality check module and calculate quality statistics
+
+    # Step 3: Organism Detection
+    # Pass forward and reverse datasets to organism detection module and return the dominate genus and species
+
+    # Step 4: Assembly (Only skesa for now)
+    # Pass forward and reverse datasets to assembly module and return a path to the results or paths to specific files
