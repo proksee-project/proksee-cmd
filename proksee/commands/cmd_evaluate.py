93c644a8f40f3ca3a230fbade8968ff5926c3b0e
Eric Marinier <emarinier@gmail.com>
2021-03-19 10:08:14 -0500

    Added basic workflow, species estimation from contigs.

--- proksee/commands/cmd_evaluate.py
+++ proksee/commands/cmd_evaluate.py
@@ -21,17 +21,85 @@ specific language governing permissions and limitations under the License.
 import click
 import os
 
+from pathlib import Path
+
+from proksee.assembly_database import AssemblyDatabase
+from proksee.assembly_measurer import AssemblyMeasurer
+from proksee.machine_learning_evaluator import MachineLearningEvaluator
+from proksee.species import Species
+from proksee.species_estimator import SpeciesEstimator
+
+DATABASE_PATH = os.path.join(Path(__file__).parent.parent.absolute(), "database",
+                             "database.csv")
+
 
 @click.command('evaluate',
                short_help='Evaluates the quality of an assembly.')
+@click.argument('contigs', required=True,
+                type=click.Path(exists=True, file_okay=True, dir_okay=False))
+@click.option('-o', '--output', required=True,
+              type=click.Path(exists=False, file_okay=False,
+                              dir_okay=True, writable=True))
 @click.pass_context
-def cli(ctx):
-    evaluate()
+def cli(ctx, contigs, output):
+    evaluate(contigs, output)
 
 
-def evaluate():
+def determine_species(contigs_filename, assembly_database, output_directory, species_name=None):
     """
-    The main control flow of the program that assembles reads.
+    Attempts to determine the species in the contigs.
+
+    ARGUMENTS:
+        contigs_filename (string): the contigs to determine the species from
+        assembly_database (AssemblyDatabase): the assembly database
+        output_directory (string): the location  of the output directory -- for placing temporary output
+        species_name (string): optional; the scientific name of the species
+
+    RETURNS:
+        species_list (List(Species)): a list of major species estimated to be present in the contigs
     """
+
+    species_list = None
+
+    if species_name:
+        if assembly_database.contains(species_name):
+            click.echo("\nThe species '" + str(species_name) + "' was recognized.")
+            species_list = [Species(species_name, 1.0)]
+
+        else:
+            click.echo("\nThe species name '" + str(species_name) + "' is unrecognized.")
+
+    if species_list is None:
+        click.echo("\nAttempting to identify the species from the contigs.")
+
+        input_file_locations = [contigs_filename]  # Needs to be a list.
+        species_estimator = SpeciesEstimator(input_file_locations, output_directory)
+        species_list = species_estimator.estimate_major_species()
+
+    return species_list
+
+
+def evaluate(contigs_filename, output_directory):
+    """
+    The main control flow of the program that evaluates the assembly.
+    """
+
+    # Species and assembly database:
+    assembly_database = AssemblyDatabase(DATABASE_PATH)
+
+    # Estimate species
+    species_list = determine_species(contigs_filename, assembly_database, output_directory, None)
+    species = species_list[0]
+    #report_species(species_list)
+    print(species.name)
+
+    # Measure assembly quality statistics:
+    assembly_measurer = AssemblyMeasurer(contigs_filename, output_directory)
+    fast_assembly_quality = assembly_measurer.measure_quality()
+
+    # Machine learning evaluation (fast assembly)
+    evaluator = MachineLearningEvaluator(species, fast_assembly_quality)
+    evaluation = evaluator.evaluate()
+    click.echo(evaluation.report)
 
     click.echo("Complete.\n")
