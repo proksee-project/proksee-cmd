a2838f22733218348285ba38042a2f7da1fe3b87
Eric Marinier <emarinier@gmail.com>
2021-04-07 10:24:33 -0500

    Adding directory creation.

--- proksee/commands/cmd_evaluate.py
+++ proksee/commands/cmd_evaluate.py
@@ -60,6 +60,10 @@ def evaluate(contigs_filename, output_directory, species_name=None):
         The contigs with passed filename will be evaluated and the results will be written to standard output.
     """
 
+    # Make output directory:
+    if not os.path.isdir(output_directory):
+        os.mkdir(output_directory)
+
     # Species and assembly database:
     assembly_database = AssemblyDatabase(DATABASE_PATH)
 
