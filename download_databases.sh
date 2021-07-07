5697b53b1b57a68b5b8b9faecca714d58a9cfc2a
Eric Marinier <emarinier@gmail.com>
2021-07-07 11:37:25 -0500

    Removing echo

--- download_databases.sh
+++ download_databases.sh
@@ -4,7 +4,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 DATABASE_DIR="$SCRIPT_DIR/proksee/database"
 MASH_SKETCH="$DATABASE_DIR/refseq.genomes.k21s1000.msh"
 
-echo $MASH_SKETCH
-
 wget -O $MASH_SKETCH https://gembox.cbcb.umd.edu/mash/refseq.genomes.k21s1000.msh
 
